0948912840e3a573d98dc0edfff0d131eded2372
Claudio Bruno <cdbruno10@hotmail.com>
2023-04-21 00:08:10 -0300

    primer comit

---
+++ header.php
@@ -0,0 +1,200 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDarkDropdown"
+          aria-controls="navbarNavDarkDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <button
+                class="btn btn-dark dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Materiales
+              </button>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <button
+                class="btn btn-dark dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Mano de obra
+              </button>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <button
+                class="btn btn-dark dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Servicios varios
+              </button>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="#">Alarmas</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Alquiler de herramientas</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#"
+                    >Automatiacion de portones</a
+                  >
+                </li>
+                <li><a class="dropdown-item" href="#">Contenedores</a></li>
+                <li><a class="dropdown-item" href="#">Deasgotes</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Empresas de limpiezas</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Fletes y mudanzas</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Hormigon elaborado</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">Movimientos de suelos</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Ploteados</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Restaurador de muebles</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">Servicios de internet</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Venta de gas</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <button
+                class="btn btn-dark dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Reparciones varias
+              </button>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="#">Cerrageria</a></li>
+                <li>
+                  <a class="dropdown-item" href="#"
+                    >Cocinas calefones y calefactores
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">Computadoras</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">Cortadoras de pasto</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Heladeras</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Lavarropas</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Microondas</a></li>
+                <li>
+                  <a class="dropdown-item" href="#"
+                    >Motosierras desmalezadoras</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">Telefonos</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">Televisores moniotores</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <button
+                class="btn btn-dark dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Profecionales
+              </button>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="#">Abogados</a></li>
+                <li><a class="dropdown-item" href="#">Agrimensores</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Arquitectos</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Contadores</a></li>
+                <li><a class="dropdown-item" href="#">Escribanos</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Ingenieros civiles</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">Empresas constructoras</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">Brokers de seguros</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <button
+                class="btn btn-dark dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Art. del hogar
+              </button>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li>
+                  <a class="dropdown-item" href="#">Articulos del hogar</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Colchoneria</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Cortinas - Ropa blaca</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Decoracion</a></li>
+                <li><a class="dropdown-item" href="#">Muebles</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Vivero</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <button
+                class="btn btn-dark dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Urgencias
+              </button>
+              <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="#">Albañileria</a></li>
+                <li><a class="dropdown-item" href="#">Electricidad</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Herreia soldadores</a>
+                </li>
+                <li><a class="dropdown-item" href="#">Pinturas</a></li>
+                <li>
+                  <a class="dropdown-item" href="#">Plomeria</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
